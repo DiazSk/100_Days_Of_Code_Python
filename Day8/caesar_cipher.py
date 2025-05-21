20dd9adf4009035736626e9a880acc8fc7584fb7
DiazSk <zaid07sk@gmail.com>
2025-05-20 22:29:00 -0700

    Add Caesar cipher implementation with encryption and decryption functions
    - Introduced caesar_cipher.py containing the logo, alphabet, and functions for encoding and decoding messages.
    - Implemented user interaction for selecting encoding/decoding and inputting text and shift values.
    - Added functionality to handle non-alphabet characters in the input text.

---
+++ Day8/caesar_cipher.py
@@ -0,0 +1,90 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+print(logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def encrypt(cipher_text, shift_amount):
+    encrypted_text = ""
+
+    for letter in cipher_text:
+        # Shifting the position of the letter by the shift amount
+        shifted_position = alphabet.index(letter) + shift_amount
+
+        # If the shifted position is greater than the length of the alphabet, wrap around to the beginning
+        shifted_position %= len(alphabet)
+
+        # Get the shifted letter from the alphabet
+        shifted_letter = alphabet[shifted_position]
+
+        # Add the shifted letter to the encrypted text
+        encrypted_text += shifted_letter
+
+    return f"Here is the encoded text: {encrypted_text}"
+
+
+def decrypt(cipher_text, shift_amount):
+    decrypted_text = ""
+    
+    for letter in cipher_text:
+        # Find the position of the letter in the alphabet and shift it back by the shift amount
+        shifted_position = alphabet.index(letter) - shift_amount
+
+        # If the shifted position is less than 0, wrap around to the end of the alphabet
+        shifted_position %= len(alphabet)
+
+        # Get the shifted letter from the alphabet
+        shifted_letter = alphabet[shifted_position]
+
+        # Add the shifted letter to the decrypted text
+        decrypted_text += shifted_letter
+
+    return f"Here is the decoded text: {decrypted_text}"
+
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+
+    for letter in original_text:
+
+        if letter not in alphabet:
+            output_text += letter
+        else:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+
+should_continue = True
+
+while should_continue:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n").lower()
+    if restart == "no":
+        should_continue = False
+        print("Goodbye")
